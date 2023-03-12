3e308912621b8e64983b6718df2900655ab8d68f
Saikiran voladri <200303124537@paruluniversity.ac.in>
2023-03-12 21:11:21 +0530

    jobo app

---
+++ app/src/main/java/com/example/jobo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.jobo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openCamera(android.view.View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } else {
+            Toast.makeText(this, "Unable to open camera.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Use the image captured by the camera
+            Uri imageUri = data.getData();
+            // Do something with the image
+        }
+    }
+}
